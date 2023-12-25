0aea16e8f849a9b69844e9a5df2dbdad21db29b8
mooseburger <carlosfabianramirez@gmail.com>
2023-12-25 11:40:43 -0400

    Correct display of no results message

--- src/components/AnswerSearch.vue
+++ src/components/AnswerSearch.vue
@@ -29,45 +29,39 @@
 <template>
     <ais-instant-search :index-name="algolia.index"
                         :search-client="searchClient">
-        <div class="row mb-4 answer-search-controls">
+        <div class="row mb-5 answer-search-controls">
             <div class="col-md-10">
                 <ais-search-box :class-names="{ 'ais-SearchBox-input': 'form-control search-control py-2', 'ais-SearchBox-submit': 'search-glass', 'ais-SearchBox-reset': 'search-reset' }" />
             </div>
             <div class="col-md-2">
-                <ais-sort-by :items="sort" :class-names="{ 'ais-SortBy-select': 'form-select search-control px-3 py-2', 'ais-SortBy-option': null }"></ais-sort-by>
+                <ais-sort-by :items="sort" :class-names="{ 'ais-SortBy-select': 'form-select search-control px-3 py-2' }"></ais-sort-by>
             </div>
         </div>
         
-        <div class="row">
-            <div class="col-12">
-                <ais-infinite-hits>
-                    <template v-slot="{
-                        items,
-                        refineNext,
-                        isLastPage
-                    }">
-                        <div class="row mb-5" v-for="a in items">
-                            <div class="col-12">
-                                <div class="card answer">
-                                    <div class="card-body">
-                                        <h1 class="answer-title mb-4"><a :href="`/answer/${a.id}`">#{{ a.id }}</a></h1>
-                                        <p class="answer-text">{{a.text}}</p>
-                                    </div>
-                                </div>
+        <ais-infinite-hits>
+            <template v-slot="{
+                items,
+                refineNext,
+                isLastPage
+            }">
+                <p v-if="!items?.length" class="no-results">No results</p>
+                <div class="row mb-5" v-for="a in items">
+                    <div class="col-12">
+                        <div class="card answer">
+                            <div class="card-body">
+                                <h1 class="answer-title mb-4"><a :href="`/answer/${a.id}`">#{{ a.id }}</a></h1>
+                                <p class="answer-text">{{a.text}}</p>
                             </div>
                         </div>
-                        <div v-if="!isLastPage" class="row justify-content-center">
-                            <div class="col-md-6 text-center">
-                                <button class="btn btn-question" @click="refineNext">Load More</button>
-                            </div>
-                        </div>
-                    </template>
-                    <template v-slot:loadMore="{ page, isLastPage, refineNext }">
-                        <div></div>
-                    </template>
-                </ais-infinite-hits>
-            </div>
-        </div>
+                    </div>
+                </div>
+                <div v-if="!isLastPage" class="row justify-content-center">
+                    <div class="col-md-6 text-center">
+                        <button class="btn btn-question" @click="refineNext">Load More</button>
+                    </div>
+                </div>
+            </template>
+        </ais-infinite-hits>
         
     </ais-instant-search>
 </template>
