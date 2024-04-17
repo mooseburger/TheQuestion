15c17425e504fb404be15370e513f690b3a3ef05
mooseburger <carlosfabianramirez@gmail.com>
2024-04-16 21:59:39 -0400

    Setup responsive vajra background

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import './scss/site.scss';
 import './assets/logo.svg';
-import './assets/circle-triangle-pattern.png';
-import './assets/circle-pattern.png';
-import './assets/six-sided-pattern.png';
 import './assets/cloud.png';
 import './assets/cloud-5.png';
 import './assets/cloud-4.png';
